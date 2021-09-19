f802972282c036ed9a49668b127105a885e2de88
Sebastián <sebastianleiva2001@gmail.com>
2021-09-18 22:01:50 -0300

    se agregan archivos al repositorio

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastController, ToastOptions, NavController } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
@@ -7,14 +12,35 @@ import { ToastController, ToastOptions, NavController } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  declarations: [HomePage]
+})
+
+
+
 export class HomePage{
 
+  //nombre : "juan";
+  //contraseña : 1234;
+
   toastOptions: ToastOptions
-  constructor(private toast: ToastController, public navCtrl: NavController) {
+  constructor(private toast: ToastController, public navCtrl: NavController, private Toast: ToastController) {  }
 
+  /*verificar(Nombre: HTMLInputElement, Contraseña: HTMLInputElement){
 
+    if (Nombre.value = this.nombre){
 
-  }
+      console.log("hola")
+    }
+
+  }*/
 
   
   async showToast() {
@@ -27,6 +53,18 @@ export class HomePage{
       
     });
     toast.present();
+  }
+
+  async ShowToast() {
+    const Toast = await this.Toast.create({
+      message: 'Su cuenta se ha creado!.',
+      duration: 2000,
+      position: "top",
+      animated: true,
+      cssClass: 'my-custom-class',
+      
+    });
+    Toast.present();
   }
 
 }
